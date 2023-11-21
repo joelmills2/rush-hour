e236d84909274a82b22e5fbca61ac0424130b26a
Joel Mills <joelarimills@gmail.com>
2023-11-20 20:01:52 -0500

    interim commit with updated constraints + classes

---
+++ print_board.py
@@ -0,0 +1,86 @@
+
+from colorama import Back, Style, Fore
+
+
+# Original board
+def create_board():
+    board = [
+        ['  ', '  ', 'V2', 'H3', 'H3', 'H3'],
+        ['V2', 'V2', 'V2', '  ', 'V2', 'V2'],
+        ['V2', 'V2', 'R ', 'R ', 'V2', 'V2'],
+        ['V2', 'H2', 'H2', 'H2', 'H2', 'V3'],
+        ['V2', '  ', '  ', '  ', '  ', 'V3'],
+        ['H2', 'H2', 'H3', 'H3', 'H3', 'V3']
+    ]
+    return board
+
+
+def create_colour_board(board):
+    # Initialize color board with empty strings
+    color_board = [['' for _ in row] for row in board]
+
+    # Helper function to assign colors to H2, H3, V2, and V3 pairs
+    colours = [getattr(Back, color) for color in dir(Back) if color.isupper()]
+    colours.remove(Back.RED)
+    colours.remove(Back.LIGHTRED_EX)
+    colours.extend(colours)
+    colours.extend(colours)
+
+    print(Style.RESET_ALL)
+    for row in range(len(board)):  # Iterate up to the last row
+        for col in range(len(board[row])):
+            if board[row][col] == 'R ':
+                color_board[row][col] = Back.RED
+            if board[row][col] == 'H3' and col + 1 < len(board[row]) and board[row][col + 1] == 'H3' and col + 2 < len(board[row]) and board[row][col + 2] == 'H3':
+                color = colours.pop()
+                color_board[row][col] = color
+                color_board[row][col + 1] = color
+                color_board[row][col + 2] = color
+                col += 2
+            elif board[row][col] == 'H2' and col + 1 < len(board[row]) and board[row][col + 1] == 'H2' and color_board[row][col] == '':
+                color = colours.pop()
+                color_board[row][col] = color
+                color_board[row][col + 1] = color
+                col += 1
+            elif board[row][col] == 'V2' and row + 1 < len(board) and board[row + 1][col] == 'V2' and color_board[row][col] == '' and color_board[row + 1][col] == '':
+                color = colours.pop()
+                color_board[row][col] = color
+                color_board[row + 1][col] = color
+            elif board[row][col] == 'V3' and row + 1 < len(board) and board[row + 1][col] == 'V3' and row + 2 < len(board) and board[row + 2][col] == 'V3' and color_board[row][col] == '' and color_board[row + 1][col] == '' and color_board[row + 2][col] == '':
+                color = colours.pop()
+                color_board[row][col] = color
+                color_board[row + 1][col] = color
+                color_board[row + 2][col] = color
+    return color_board
+
+def display_board(board, color_board):
+    display_board = []
+    for row in range(6):
+        display_row = []
+        for col in range(6):
+            display_row.append(f'{color_board[row][col]}{board[row][col]}{Style.RESET_ALL}')
+        display_board.append(display_row)
+    return display_board
+
+def print_table(final_board):
+    print("╒════╤════╤════╤════╤════╤════╕")
+    for row in range(6):
+        for col in range(5):
+            print("│",final_board[row][col], "", end="")
+        if row == 2:
+            print("│",final_board[row][5], "")
+        else:
+            print("│",final_board[row][5], "│")
+        if row == 5:
+            print("╘════╧════╧════╧════╧════╧════╛")
+        else:
+            print("├────┼────┼────┼────┼────┼────┤")
+
+# Assign colors to H2, H3, V2, and V3 pairs
+board = create_board()
+color_board = create_colour_board(board)
+final_board = display_board(board, color_board)
+
+
+print("Display Board:")
+print_table(final_board)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
@@ -8,76 +9,214 @@ config.sat_backend = "kissat"
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class RushHourPropositions:
+# Grid dimensions
+GRID_SIZE = 6
 
-    def __init__(self, data):
-        self.data = data
+# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
+# Proposition classes for each type of cell content
+@proposition(E)
+class Empty:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
 
     def __repr__(self):
-        return f"A.{self.data}"
+        return f"Empty({self.i},{self.j})"
+
+@proposition(E)
+class Red:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"Red({self.i},{self.j})"
+
+@proposition(E)
+class H2:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"H2({self.i},{self.j})"
+   
+@proposition(E)
+class H3:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"H3({self.i},{self.j})"
+   
+@proposition(E)
+class V2:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"V2({self.i},{self.j})"
+   
+@proposition(E)
+class V3:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"V3({self.i},{self.j})"
+   
+@proposition(E)
+class CMR:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"CMR({self.i},{self.j})"
+
+@proposition(E)
+class CML:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"CML({self.i},{self.j})"
+        
+   
+@proposition(E)
+class CMU:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"CMU({self.i},{self.j})"
+   
+@proposition(E)
+class CMD:
+    def __init__(self, i, j):
+        self.i, self.j = i, j
+
+    def __repr__(self):
+        return f"CMD({self.i},{self.j})"
+
+# Innitialize Empty Grid
+def create_grid():
+    new_grid = [[{} for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+    return new_grid
+
+# Fill Grid with propositions
+def fill_grid(grid):
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            grid[i][j]["E"] = Empty(i, j)
+            grid[i][j]["H2"] = H2(i, j)
+            grid[i][j]["H3"] = H3(i, j)
+            grid[i][j]["V2"] = V2(i, j)
+            grid[i][j]["V3"] = V3(i, j)
+            grid[i][j]["CMR"] = CMR(i, j)
+            grid[i][j]["CML"] = CML(i, j)
+            grid[i][j]["CMU"] = CMU(i, j)
+            grid[i][j]["CMD"] = CMD(i, j)
+            if i == 2:
+                grid[i][j]["Red"] = Red(i, j)
+    return grid
 
 
-grid_size = 6
-propositions = ['E', 'RED', 'H2', 'H3', 'V2', 'V3', 'CMR', 'CML', 'CMD', 'CMU']
+# CONSTRAINT FUNCTIONS
+def only_one_state(grid):
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
 
-# Initialize an empty dictionary for the grid
-prev_grid = {}
-next_grid = {}
+            if i == 2:
+                constraint.add_exactly_one(E, [
+                    grid[i][j]["E"],
+                    grid[i][j]["H2"],
+                    grid[i][j]["H3"],
+                    grid[i][j]["V2"],
+                    grid[i][j]["V3"],
+                    grid[i][j]["Red"]
+                ])
 
-# Iterate through each row and column in the grid
-for i in range(grid_size):
-    for j in range(grid_size):
-        
-        # Initialize an empty dictionary for the propositions of the current cell
-        cell_prev_propositions = {}
-        
-        # Iterate through each proposition identifier
-        for prop in propositions:
-            
-            # Create a new instance of RushHourPropositions for the current proposition
-            # The identifier is created by concatenating the coordinates and proposition identifier
-            proposition_object = RushHourPropositions(f"{prop}{i}{j}")
-            
-            # Add the new RushHourPropositions instance to the cell_propositions dictionary
-            cell_prev_propositions[prop] = proposition_object
-        
-        # Once all propositions for the current cell have been created,
-        # add the cell_propositions dictionary to the grid dictionary
-        prev_grid[(i, j)] = cell_prev_propositions
+            else:
+                constraint.add_exactly_one(E, [
+                    grid[i][j]["E"],
+                    grid[i][j]["H2"],
+                    grid[i][j]["H3"],
+                    grid[i][j]["V2"],
+                    grid[i][j]["V3"]
+                ])
 
-# Iterate through each row and column in the grid
-for i in range(grid_size):
-    for j in range(grid_size):
-        
-        # Initialize an empty dictionary for the propositions of the current cell
-        cell_next_propositions = {}
-        
-        # Iterate through each proposition identifier
-        for prop in propositions:
-            
-            # Create a new instance of RushHourPropositions for the current proposition
-            # The identifier is created by concatenating the coordinates and proposition identifier
-            proposition_object = RushHourPropositions(f"{prop}{i}{j}")
-            
-            # Add the new RushHourPropositions instance to the cell_propositions dictionary
-            cell_next_propositions[prop] = proposition_object
-        
-        # Once all propositions for the current cell have been created,
-        # add the cell_propositions dictionary to the grid dictionary
-        next_grid[(i, j)] = cell_next_propositions
+def can_move(grid):
+    for i in range(GRID_SIZE):
+        for j in range (GRID_SIZE):
+            if (i < GRID_SIZE-1):
+                x = grid[i][j]['H2']
+                y = grid[i+1][j]['E']
+                z = grid[i][j]['CMR']
+                E.add_constraint((~x | ~y) | z)
 
-# Function to print a grid
-def print_grid(grid, grid_name):
-    print(f"Grid: {grid_name}")
-    # Iterate through each cell in the grid
-    for coords, cell_propositions in grid.items():
-        # Iterate through each proposition in the current cell
-        for prop_id, prop_object in cell_propositions.items():
-            # Print the coordinates, proposition identifier, and proposition object identifier
-            print(f"Coordinates: {coords}, Proposition Identifier: {prop_id}")
-    print("\n")  # Print a newline between grids for readability
+            if (i < GRID_SIZE-2):
+                E.add_constraint((~(grid[i][j]['H3']) | ~(grid[i+1][j]['E'])) | (grid[i][j]['CMR']))
+           
+            if (i > 0):
+                E.add_constraint((~grid[i][j]['H2'] | ~grid[i-1][j]['E']) | grid[i][j]['CML'])
+
+            if (i > 1):
+                E.add_constraint((~grid[i][j]['H3'] | ~grid[i-1][j]['E']) | grid[i][j]['CML'])
+
+            if (j < GRID_SIZE-1):
+                E.add_constraint((~grid[i][j]['V2'] | ~grid[i][j+1]['E']) | grid[i][j]['CMD'])
+
+            if (j < GRID_SIZE-2):
+                E.add_constraint((~grid[i][j]['V3'] | ~grid[i][j+1]['E']) | grid[i][j]['CMD'])
+                                                                           
+            if (j > 0):
+                E.add_constraint((~grid[i][j]['V2'] | ~grid[i][j-1]['E']) | grid[i][j]['CMU'])
+
+            if (j > 1):
+                E.add_constraint((~grid[i][j]['V3'] | ~grid[i][j-1]['E']) | grid[i][j]['CMU'])
+
+
+# Building Initial Board:
+def starting_board(grid):
+    E.add_constraint(grid[0][0]['E'])
+    E.add_constraint(grid[0][1]['E'])
+    E.add_constraint(grid[0][2]['V2'])
+    E.add_constraint(grid[0][3]['H3'])
+    E.add_constraint(grid[0][4]['H3'])
+    E.add_constraint(grid[0][5]['H3'])
+    E.add_constraint(grid[1][0]['V2'])
+    E.add_constraint(grid[1][1]['V2'])
+    E.add_constraint(grid[1][2]['V2'])
+    E.add_constraint(grid[1][3]['E'])
+    E.add_constraint(grid[1][4]['V2'])
+    E.add_constraint(grid[1][5]['V2'])
+    E.add_constraint(grid[2][0]['V2'])
+    E.add_constraint(grid[2][1]['V2'])
+    E.add_constraint(grid[2][2]['Red'])
+    E.add_constraint(grid[2][3]['Red'])
+    E.add_constraint(grid[2][4]['V2'])
+    E.add_constraint(grid[2][5]['V2'])
+    E.add_constraint(grid[3][0]['V2'])
+    E.add_constraint(grid[3][1]['H2'])
+    E.add_constraint(grid[3][2]['H2'])
+    E.add_constraint(grid[3][3]['H2'])
+    E.add_constraint(grid[3][4]['H2'])
+    E.add_constraint(grid[3][5]['V3'])
+    E.add_constraint(grid[4][0]['V2'])
+    E.add_constraint(grid[4][1]['E'])
+    E.add_constraint(grid[4][2]['E'])
+    E.add_constraint(grid[4][3]['E'])
+    E.add_constraint(grid[4][4]['E'])
+    E.add_constraint(grid[4][5]['V3'])
+    E.add_constraint(grid[5][0]['H2'])
+    E.add_constraint(grid[5][1]['H2'])
+    E.add_constraint(grid[5][2]['H3'])
+    E.add_constraint(grid[5][3]['H3'])
+    E.add_constraint(grid[5][4]['H3'])
+    E.add_constraint(grid[5][5]['V3'])
+
+
+#Create Previous Grid
+prev_grid = create_grid()
+prev_grid = fill_grid(prev_grid)
+
 
 
 # Build an example full theory for your setting and return it.
@@ -86,195 +225,20 @@ def print_grid(grid, grid_name):
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 def example_theory():
- 
-    # Each cell can only one truth value on a given move, and a cell with no cars cannot move
-    for i in range (0,6):
-        for j in range (0,6):
-            constraint.add_exactly_one(
-                E, 
-                prev_grid[(i, j)]['E'],
-                prev_grid[(i, j)]['RED'],
-                prev_grid[(i, j)]['H2'],
-                prev_grid[(i, j)]['H3'],
-                prev_grid[(i, j)]['V2'],
-                prev_grid[(i, j)]['V3']
-             )
-            constraint.add_exactly_one(
-                E, 
-                next_grid[(i, j)]['E'],
-                next_grid[(i, j)]['RED'],
-                next_grid[(i, j)]['H2'],
-                next_grid[(i, j)]['H3'],
-                next_grid[(i, j)]['V2'],
-                next_grid[(i, j)]['V3']
-             )
-            constraint.add_exactly_one(
-                E,
-                prev_grid[(i, j)]['E'],
-                prev_grid[(i, j)]['CMR'],
-                prev_grid[(i, j)]['CML'],
-                prev_grid[(i, j)]['CMD'],
-                prev_grid[(i, j)]['CMU']
-            )
-            constraint.add_exactly_one(
-                E,
-                next_grid[(i, j)]['E'],
-                next_grid[(i, j)]['CMR'],
-                next_grid[(i, j)]['CML'],
-                next_grid[(i, j)]['CMD'],
-                next_grid[(i, j)]['CMU']
-            )
-
-
-    # Each car has a fixed length of 2 or 3 - if a cell has a car, one or more adjacent cells must have the same car (all cars have a height of 1)
-    # Horizontal cars length 2
-    for row in range(grid_size):
-        for col in range(grid_size - 1):  # Subtract 1 to avoid index out of range
-            E.add_constraint(
-                prev_grid[(row, col)]['H2'] >> prev_grid[(row, col + 1)]['H2']
-            )
-
-    for row in range(grid_size):
-        for col in range(grid_size - 1):  # Subtract 1 to avoid index out of range
-            E.add_constraint(
-                next_grid[(row, col)]['H2'] >> next_grid[(row, col + 1)]['H2']
-            )
-
-    # Horizontal cars length 3
-    for row in range(grid_size):
-        for col in range(grid_size - 2):
-            E.add_constraint(
-                prev_grid[(row, col)]['H3'] >> (prev_grid[(row, col + 1)]['H3'] & prev_grid[(row, col + 2)]['H3'])
-            )
-    
-    for row in range(grid_size):
-        for col in range(grid_size - 2):
-            E.add_constraint(
-                next_grid[(row, col)]['H3'] >> (next_grid[(row, col + 1)]['H3'] & next_grid[(row, col + 2)]['H3'])
-            )
-            
-
-    # Vertical cars length 2
-    for col in range(grid_size):
-        for row in range(grid_size - 1):
-            E.add_constraint(
-                prev_grid[(row, col)]['V2'] >> prev_grid[(row + 1, col)]['V2']
-            )
-    
-    for col in range(grid_size):
-        for row in range(grid_size - 1):
-            E.add_constraint(
-                next_grid[(row, col)]['V2'] >> next_grid[(row + 1, col)]['V2']
-            )
-
-    # Vertical cars length 3
-    for col in range(grid_size):
-        for row in range(grid_size - 2):
-            E.add_constraint(
-                prev_grid[(row, col)]['V3'] >> (prev_grid[(row + 1, col)]['V3'] & prev_grid[(row + 2, col)]['V3'])
-            )
-    
-    for col in range(grid_size):
-        for row in range(grid_size - 2):
-            E.add_constraint(
-                next_grid[(row, col)]['V3'] >> (next_grid[(row + 1, col)]['V3'] & next_grid[(row + 2, col)]['V3'])
-            )
-
-
-    # The red car is a horizontal car of length 2, and must be in the 3rd row (i = 2)
-    constraint.add_exactly_one(
-        E,
-        (prev_grid[(2,0)]['RED'] & prev_grid(2,1)['RED']),
-        (prev_grid[(2,1)]['RED'] & prev_grid(2,2)['RED']),
-        (prev_grid[(2,2)]['RED'] & prev_grid(2,3)['RED']),
-        (prev_grid[(2,3)]['RED'] & prev_grid(2,4)['RED']),
-        (prev_grid[(2,4)]['RED'] & prev_grid(2,5)['RED'])
-    )
-    constraint.add_exactly_one(
-        E,
-        (next_grid[(2,0)]['RED'] & next_grid(2,1)['RED']),
-        (next_grid[(2,1)]['RED'] & next_grid(2,2)['RED']),
-        (next_grid[(2,2)]['RED'] & next_grid(2,3)['RED']),
-        (next_grid[(2,3)]['RED'] & next_grid(2,4)['RED']),
-        (next_grid[(2,4)]['RED'] & next_grid(2,5)['RED'])
-    )
-    # No vehicles moving horizontally can be in the same row as the red car, ie. i != 2
-
-    # The game is complete when no cars are blocking the red car from exiting the board, ie. R_{2j} > P_{2j} for all j in [0,5]
-
-    # When one part of a car moves, the rest of the car must move with it, ie. H3_{10} && H3_{11} && H3_{12} -> H3_{12} && H3_{13} && H3_{14}
-  
-    # Checking if a car has any valid moves
-    for i in range(grid_size):
-        for j in range (grid_size):
-            if (i < 5):
-                (prev_grid[(i, j)]['H2'] & prev_grid[(i+1, j)]['E']) >> prev_grid[(i, j)]['CMR']
-
-            if (i < 4):
-                (prev_grid[(i, j)]['H3'] & prev_grid[(i+1, j)]['E']) >> prev_grid[(i, j)]['CMR']
-            if (i > 0):
-                (prev_grid[(i, j)]['H2'] & prev_grid[(i-1, j)]['E']) >> prev_grid[(i, j)]['CML']
-
-            if (i > 1):
-                (prev_grid[(i, j)]['H3'] & prev_grid[(i-1, j)]['E']) >> prev_grid[(i, j)]['CML']
-
-
-            if (j < 5):
-
-                (prev_grid[(i, j)]['V2'] & prev_grid[(i, j+1)]['E']) >> prev_grid[(i, j)]['CMD']
-
-            if (j < 4):
-
-                (prev_grid[(i, j)]['V3'] & prev_grid[(i, j+1)]['E']) >> prev_grid[(i, j)]['CMD']
-                                                                           
-            if (j > 0):
-
-                (prev_grid[(i, j)]['V2'] & prev_grid[(i, j-1)]['E']) >> prev_grid[(i, j)]['CMU']
-
-            if (j > 1): 
-
-                (prev_grid[(i, j)]['V3'] & prev_grid[(i, j-1)]['E']) >> prev_grid[(i, j)]['CMU']
-    
-
-
-    # Simulating the movement of a car; updating the grid
-
-    no_change = False
-    grid_list = [] # Stores all next_grid options to be outputed
-    move_counter = 0 # How many moves can be made
-            
-    for i in range(grid_size): 
-        for j in range (grid_size):
-
-            no_change = False
-            print ('i', i, 'j', j)
-            print (prev_grid[(i, j)]['E'])
-            # The same spot cannnot have PCMR and PCML as these propositions are only true for the lead car.
-            # The same spot cannnot have PCMR and PCMD as these propositions can only be treu for cells of different orientations.
-            if (0 < i < grid_size - 1):
-                prev_grid[(i, j)]['CMR'] & prev_grid[(i, j)]['H2'] >> next_grid[(i+1, j)]['H2'] & next_grid[(i, j)]['H2'] & next_grid[(i-1, j)]['E']
-                prev_grid[(i, j)]['CML'] & prev_grid[(i, j)]['H2'] >> next_grid[(i-1, j)]['H2'] & next_grid[(i, j)]['H2'] & next_grid[(i+1, j)]['E']
-
-            if (1 < i < grid_size - 2):
-                prev_grid[(i, j)]['CMR'] & prev_grid[(i, j)]['H3'] >> next_grid[(i+1, j)]['H3'] & next_grid[(i, j)]['H3'] & next_grid[(i-1, j)]['H3'] & next_grid[(i-2, j)]['E']
-                prev_grid[(i, j)]['CML'] & prev_grid[(i, j)]['H3'] >> next_grid[(i-1, j)]['H3'] & next_grid[(i, j)]['H3'] & next_grid[(i+1, j)]['H3'] & next_grid[(i+2, j)]['E']              
-
-            if (0 < j < grid_size - 1):
-                prev_grid[(i, j)]['CMD'] & prev_grid[(i, j)]['V2'] >> next_grid[(i, j+1)]['V2'] & next_grid[(i, j)]['V2'] & next_grid[(i, j-1)]['E']
-                prev_grid[(i, j)]['CMU'] & prev_grid[(i, j)]['V2'] >> next_grid[(i, j-1)]['V2'] & next_grid[(i, j)]['V2'] & next_grid[(i, j+1)]['E']
-
-            if (1 < j < grid_size - 2):
-                prev_grid[(i, j)]['CMD'] & prev_grid[(i, j)]['V3'] >> next_grid[(i, j+1)]['V3'] & next_grid[(i, j)]['V3'] & next_grid[(i, j-1)]['V3'] & next_grid[(i, j-2)]['E']
-                prev_grid[(i, j)]['CMU'] & prev_grid[(i, j)]['V3'] >> next_grid[(i, j-1)]['V3'] & next_grid[(i, j)]['V3'] & next_grid[(i, j+1)]['V3'] & next_grid[(i, j+2)]['E']
-
-            if (prev_grid[(i,j)]['CMR']|prev_grid[(i,j)]['CML']|prev_grid[(i,j)]['CMD']|prev_grid[(i,j)]['CMU']):
-                no_change == True
-
-            if ~no_change:
-                grid_list.append(next_grid)
-                move_counter+=1
-
+    x = prev_grid[0][0]['E']
+    y = prev_grid[0][1]['CMR']
+    print(x)
+    print(y)
+    E.add_constraint(x | y)
+    print(E)
+    input()
+   
+    can_move(prev_grid)
+    only_one_state(prev_grid)
+    # If we choose to loop through the game this would be if on first itteration
+    starting_board(prev_grid)
     return E
+
 
 if __name__ == "__main__":
 
@@ -287,11 +251,16 @@ if __name__ == "__main__":
     print("# Solutions: %d" % count_solutions(T))
     print("   Solution: %s" % T.solve())
 
+    # T.solve() returns one solution. We need a way to access them all. Read bauhaus documentation.
+    # Once we have all the solutions we can output every moveable peice and the direction it can move by outputting the i,j for when CMR, CMD, DMU, CML ho;d
+   
+
+
+""" SAVED FOR TROUBLESHOOTING
     print("\nVariable likelihoods:")
-    for i in range(grid_size):
-        for j in range(grid_size):
-            for prop in previous_propositions:
-                prop_var = prev_grid[(i, j)][prop]
-                likelihood_value = likelihood(T, prop_var)
-                print(f" {prop_var}: {likelihood_value:.2f}")
+    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+        # Ensure that you only send these functions NNF formulas
+        # Literals are compiled to NNF here
+        print(" %s: %.2f" % (vn, likelihood(T, v)))
     print()
+"""
